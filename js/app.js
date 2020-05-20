1bc3a940fd21ef2bd410aec6beeb459ee74a2ca4
Roberto Cervantes <robert.hct@gmail.com>
2020-05-19 22:16:56 -0500

    Fuciones para los botones de enviar y reset, muestra elementos en el DOM

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const email = document.getElementById('email');
 const asunto = document.getElementById('asunto');
 const mensaje = document.getElementById('mensaje');
 const enviarBtn = document.getElementById('enviar');
+const formularioEnviar = document.getElementById('enviar-mail');
+const resetBtn = document.getElementById('resetBtn');
 
 //Listeners
 (() => {
@@ -15,6 +17,10 @@ const enviarBtn = document.getElementById('enviar');
   mensaje.addEventListener('blur', validarCampo);
 
   //Botón de enviar en el submit
+  formularioEnviar.addEventListener('submit', enviarEmail);
+
+  //Botón de reset formulario
+  resetBtn.addEventListener('click', resetFormulario);
   
 })();
 
@@ -43,6 +49,31 @@ function validarCampo() {
   }
 }
 
+//Cuando se envía el correo
+function enviarEmail(form) {
+  form.preventDefault();
+  //Spinner al presionar enviar
+  const spinnerGif = document.getElementById('spinner');
+  spinnerGif.style.display = 'block';
+
+  //gif enviado
+  const enviado = document.createElement('img');
+  enviado.src = 'img/mail.gif';
+  enviado.style.display = 'block';
+
+  //ocultar spinner y motrar enviado
+  setTimeout(() => {
+    spinnerGif.style.display = 'none';
+    spinnerGif.parentElement.appendChild(enviado);
+    setTimeout(() => {
+      enviado.remove();
+      formularioEnviar.reset();
+    }, 3000);
+  }, 2000);
+}
+
+
+
 // Función para validar la lingitud de un campo, si está o no vacío
 function validarLongitud(campo) {  
   if (campo.value.length > 0) {
@@ -66,4 +97,10 @@ function validarEmail(email) {
     email.style.borderBottomColor = 'red';    
     email.classList.add('error');
   }
+}
+
+//Función para resetear formulario
+function resetFormulario(e) {
+  e.preventDefault();
+  formularioEnviar.reset();
 }
